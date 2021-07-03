18d3a6a33454339f53dde18fb873d000073b6a22
wsm9175 <76678911+wsm9175@users.noreply.github.com>
2021-07-04 00:30:22 +0900

    build structure

---
+++ app/src/main/java/com/udangtangtang/emotion_mapfile/presenter/PlusEmotionPresenter.java
@@ -0,0 +1,14 @@
+package com.udangtangtang.emotion_mapfile.presenter;
+
+import android.content.Context;
+
+import com.udangtangtang.emotion_mapfile.view.PlusEmotion;
+
+public class PlusEmotionPresenter {
+    private Context context;
+
+    public PlusEmotionPresenter(Context context){
+        this.context = context;
+    }
+
+}
---
+++ app/src/main/java/com/udangtangtang/emotion_mapfile/view/Comment_list.java
@@ -0,0 +1,44 @@
+package com.udangtangtang.emotion_mapfile.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.udangtangtang.emotion_mapfile.R;
+import com.udangtangtang.emotion_mapfile.adapter.Comment_adapter;
+import com.udangtangtang.emotion_mapfile.presenter.CommentListPresenter;
+
+import java.util.ArrayList;
+
+public class Comment_list extends Activity {
+    private TextView txt_city;
+    private RecyclerView recyclerView;
+    private Comment_adapter comment_adapter;
+    private CommentListPresenter commentListPresenter;
+    private ArrayList<String> comment_list = new ArrayList<String>();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        initView();
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        comment_adapter = new Comment_adapter(comment_list);
+        recyclerView.setAdapter(comment_adapter);
+    }
+
+    private void initView(){
+        //뷰 세팅
+       recyclerView = findViewById(R.id.recyclerview);
+       txt_city = findViewById(R.id.txt_city);
+    }
+
+
+}
