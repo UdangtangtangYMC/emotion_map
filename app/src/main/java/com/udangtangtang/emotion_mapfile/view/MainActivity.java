e6066f57d176b9007be140c6f604142d4ff4ee6e
신현호 <47271188+hyeonho9877@users.noreply.github.com>
2021-07-03 21:35:53 +0900

    ui design

--- app/src/main/java/com/udangtangtang/emotion_mapfile/view/MainActivity.java
+++ app/src/main/java/com/udangtangtang/emotion_mapfile/view/MainActivity.java
@@ -3,9 +3,14 @@ package com.udangtangtang.emotion_mapfile.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.graphics.Paint;
 import android.os.Bundle;
 
 import com.udangtangtang.emotion_mapfile.R;
+import com.udangtangtang.emotion_mapfile.domain.EmotionDTO;
+
+import java.security.cert.PKIXRevocationChecker;
+import java.util.Optional;
 
 public class MainActivity extends Activity {
 
@@ -13,5 +18,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setAntiAlias(true);
+
     }
 }
